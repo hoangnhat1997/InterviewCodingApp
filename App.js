5704aba874133b317378acc70d4cf18269c78f66
NhatPham <nhatpham@192.168.2.18>
2023-07-30 21:35:53 +0700

    First code

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import {
+  FlatList,
+  RefreshControl,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDataApi = async (page = 0) => {
+    setLoading(true);
+    return await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=20`
+    )
+      .then((res) => res.json())
+      .then((json) => {})
+      .catch((err) => console.log(err));
+  };
+
+  const getDataApi = async() =>{
+    const response = await fetchDataApi()
+    
+  }
+
+  const onRefresh = () => {};
+
+  useEffect(() => {
+    getDataApi();
+  }, []);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={{ margin: 10 }}>
+        <Text style={{ fontSize: 20, color: "#949" }}>{item.title}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          onEndReached={() => getDataApi(data.length / 20 + 1)}
+          onEndReachedThreshold={0.1}
+          refreshControl={
+            <RefreshControl
+              refreshing={loading}
+              tintColor="red"
+              onRefresh={onRefresh}
+            />
+          }
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
